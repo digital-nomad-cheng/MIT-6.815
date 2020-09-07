658d0d1ed9f0725c755fdf3ec29b671709de8262
vincent <vincentcheng929@gmail.com>
2020-09-07 21:07:52 +0800

    Update solution for pset01

--- psets/pset01/a1.cpp
+++ psets/pset01/a1.cpp
@@ -21,12 +21,77 @@ Image brightness(const Image &im, float factor)
     Image output = Image(im.width(), im.height(), im.channels());
     // image are stored using w -> h -> c order 
     // these three for loops increase memory coherence
-    for (int c = 0; c <= im.channels(); c++) {
-        for (int h = 0; h <= im.height(); h++) {
-            for (int w = 0; w <= im.width(); w++) {
+    for (int c = 0; c < im.channels(); c++) {
+        for (int h = 0; h < im.height(); h++) {
+            for (int w = 0; w < im.width(); w++) {
                 output(w, h, c) = im(w, h, c) * factor;
             }
         }
     }
     return output;
+}
+
+Image contrast(const Image &im, float factor, float midpoint)
+{
+    // Iout = factor * (Iin - midpoint) + midpoint;
+    Image output = Image(im.width(), im.height(), im.channels());
+    // image are stored using w -> h -> c order 
+    // these three for loops increase memory coherence
+    for (int c = 0; c < im.channels(); c++) {
+        for (int h = 0; h < im.height(); h++) {
+            for (int w = 0; w < im.width(); w++) {
+                output(w, h, c) = (im(w, h, c) - midpoint) * factor + midpoint;
+            }
+        }
+    }
+    return output;
+}
+
+Image color2gray(const Image &im, const std::vector<float> &weights)
+{
+    Image output = Image(im.width(), im.height(), 1);
+    // image are stored using w -> h -> c order 
+    // these three for loops increase memory coherence
+    for (int h = 0; h < im.height(); h++) {
+        for (int w = 0; w < im.width(); w++) {
+            output(w, h) = im(w, h, 0) * weights[0] + im(w, h, 1) * weights[1] +  im(w, h, 2) * weights[2];
+        }
+    }
+    return output;
+}
+
+std::vector<Image> lumiChromi(const Image &im)
+{
+    std::vector<Image> result;
+    Image grayscale = color2gray(im);
+    Image chrom = Image(im.width(), im.height(), im.channels());
+    for (int h = 0; h < im.height(); h++) {
+        for (int w = 0; w < im.width(); w++) {
+            chrom(w, h, 0) = im(w, h, 0) / grayscale(w, h);
+            chrom(w, h, 1) = im(w, h, 1) / grayscale(w, h);
+            chrom(w, h, 2) = im(w, h, 2) / grayscale(w, h);
+        }
+    }
+    result.push_back(grayscale);
+    result.push_back(chrom);
+    return result;
+}
+
+Image brightnessContrastLumi(const Image &im, float brightF, float contrastF, float midpoint)
+{
+    std::vector<Image> images = lumiChromi(im);
+    Image lumi = images[0];
+    Image chrom = images[1];
+    lumi = brightness(lumi, brightF);
+    lumi = contrast(lumi, contrastF, midpoint);
+
+    Image result = Image(im.width(), im.height(), im.channels());
+    for (int h = 0; h < im.height(); h++) {
+        for (int w = 0; w < im.width(); w++) {
+            result(w, h, 0) = chrom(w, h, 0) / lumi(w, h);
+            result(w, h, 1) = chrom(w, h, 0) / lumi(w, h);
+            result(w, h, 2) = chrom(w, h, 0) / lumi(w, h);
+        }
+    }
+    return result;
 }
--- psets/pset01/a1_main.cpp
+++ psets/pset01/a1_main.cpp
@@ -5,7 +5,13 @@ using namespace std;
 // This is a way for you to test your functions. 
 // We will only grade the contents of a0.cpp and Image.cpp
 int main() {
-	Image input = Image('./Input/boston_low_contrast.png');
-	Image output = brightness(input, 0.2);
-	output.debug_write();
+	Image input = Image("../Input/boston_low_contrast.png");
+	Image output1 = brightness(input, 1.2);
+	Image output2 = contrast(input, 2.0);
+	Image output3 = color2gray(input);
+	Image output4 = brightnessContrastLumi(input, 1.5, 2);
+	output1.write("../Output/boston_brightness.png");
+	output2.write("../Output/boston_high_contrast.png");
+	output3.write("../Output/boston_gray.png");
+	output4.write("../Output/boston_brightness_contrast_lumi.png");
 }
