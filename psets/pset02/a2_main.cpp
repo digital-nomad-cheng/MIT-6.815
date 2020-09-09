e1b6df441ae9c10f566b5fde70626d0c7647e86b
vincent <vincentcheng929@gmail.com>
2020-09-09 16:20:55 +0800

    Update solution for pset02

--- psets/pset02/a2_main.cpp
+++ psets/pset02/a2_main.cpp
@@ -10,26 +10,9 @@ int main() {
 	Image output1 = boxBlur(input, 3, true);
     output1.write("../Output/boston_box_blur.png");
 
+    Image output2 = boxBlur_filterClass(input, 3, true);
+    output2.write("../Output/boston_filter_class_box_blur.png");
 
-    
-	Image output2 = contrast(input, 2.0);
-	Image output3 = color2gray(input);
-	std::vector<Image> outputs = lumiChromi(input);
-	Image output4 = brightnessContrastLumi(input, 1.5, 2);
-	Image output5 = saturate(input, 1.5);
-	Image output6 = add_black_dot(input);
-	std::vector<Image> spanishs = spanish(input);
-	Image output7 = grayworld(input);
 
-	output1.write("../Output/boston_brightness.png");
-	output2.write("../Output/boston_high_contrast.png");
-	output3.write("../Output/boston_gray.png");
-	output4.write("../Output/boston_brightness_contrast_lumi.png");
-	output5.write("../Output/boston_saturate.png");
-	output6.write("../Output/boston_black_dot.png");
-	output7.write("../Output/boston_gray_world.png");
-	outputs[0].write("../Output/boston_lumi.png");
-	outputs[1].write("../Output/boston_chrom.png");
-	spanishs[0].write("../Output/boston_spanish_1.png");
-	spanishs[1].write("../Output/boston_spanish_2.png");
+
 }
--- psets/pset02/filtering.cpp
+++ psets/pset02/filtering.cpp
@@ -1,18 +1,83 @@
 #include "filtering.hpp"
 
+// ---------------------------- Filter ---------------------------- //
+
+// Filter Constructor
+Filter::Filter(const vector<float> &fData, int fWidth, int fHeight){
+	if (fData.size() != fWidth*fHeight) {
+		throw InvalidArgument();
+	}
+	kernel.resize(fWidth*fHeight);
+	for (int i = 0; i < fData.size(); i++) {
+		kernel[i] = fData[i];
+	}
+	this->width = fWidth;
+	this->height = fHeight;
+}
+
+Filter::Filter(int fWidth, int fHeight)
+{
+	kernel.resize(fWidth*fHeight);
+	for (int i = 0; i < kernel.size(); i++) {
+		kernel[i] = 0.0;
+	}
+	this->width = fWidth;
+	this->height = fHeight;
+}
+
+
+Filter::~Filter()
+{
+
+}
+
+// function to convolve your filter with an image
+Image Filter::convolve(const Image &im, bool clamp)
+{
+	Image output(im.width(), im.height(), im.channels());
+	int xx = this->width/2;
+	int yy = this->height/2;
+	for (int h = 0; h < im.height(); h++) {
+		for (int w = 0; w < im.width(); w++) {
+			for (int y = -yy; y <= yy; y++) {
+				for (int x = -xx; x <= xx; x++) {
+					output(w, h, 0) += this->kernel[this->width*y + x] * im.smartAccessor(w+x, h+y, 0, clamp);
+					output(w, h, 1) += this->kernel[this->width*y + x] * im.smartAccessor(w+x, h+y, 1, clamp);
+					output(w, h, 2) += this->kernel[this->width*y + x] * im.smartAccessor(w+x, h+y, 2, clamp);
+				}
+			}
+		}
+	}
+	return output;
+}
+    
+// Accessors of the filter values
+const float & Filter::operator()(int x, int y) const
+{
+	return this->kernel[this->width*y + x];
+}
+
+float & Filter::operator()(int x, int y) {
+	return this->kernel[this->width*y + x];
+}
+
+
+
+// --------------------------- Filter -------------------------- //
+
 Image boxBlur(const Image &im, int k, bool clamp)
 {
 	Image output = Image(im.width(), im.height(), im.channels());
 	int r = k / 2;
-	int weight = k * k;
+	float weight = k * k;
 
 	for (int h = 0; h < im.height(); h++) {
 		for (int w = 0; w < im.width(); w++) {
 			for (int y = -r; y <=r; y++) {
 				for (int x = -r; x <= r; x++) {
-					output(w, h, 0) += im.smartAccessor(w+x, h+y, 0) / weight;
-					output(w, h, 1) += im.smartAccessor(w+x, h+y, 1) / weight;
-					output(w, h, 2) += im.smartAccessor(w+x, h+y, 2) / weight;
+					output(w, h, 0) += im.smartAccessor(w+x, h+y, 0, clamp) / weight;
+					output(w, h, 1) += im.smartAccessor(w+x, h+y, 1, clamp) / weight;
+					output(w, h, 2) += im.smartAccessor(w+x, h+y, 2, clamp) / weight;
 				}
 			}
 		}
@@ -20,7 +85,15 @@ Image boxBlur(const Image &im, int k, bool clamp)
 	return output;
 }
 
-
+Image boxBlur_filterClass(const Image &im, int k, bool clamp)
+{
+	std::vector<float> kernel;
+	for (int i = 0; i < k*k; i++) {
+		kernel.push_back(1.0/(float(k*k)));
+	}
+	Filter box_filter = Filter(kernel, k, k);
+	return box_filter.convolve(im, clamp);
+}
 
 /**************************************************************
  //               DON'T EDIT BELOW THIS LINE                //
