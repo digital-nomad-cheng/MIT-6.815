bce4620f733f7152c7870c0063f546c297d9918c
vincent <vincentcheng929@gmail.com>
2020-09-15 20:28:37 +0800

    Update solution for pset03

--- psets/pset03/a3_main.cpp
+++ psets/pset03/a3_main.cpp
@@ -9,13 +9,14 @@ using namespace std;
 // This is a way for you to test your functions. 
 // We will only grade the contents of a0.cpp and Image.cpp
 int main() {
+    /*
 	// Denoise
     vector<Image> seq;
-    int n_images = 4;
+    int n_images = 44;
     for (int i = 1; i <= n_images; ++i) {
         ostringstream fname;
-        // fname << "./Input/aligned-ISO400/1D2N-iso400-under-";
-        fname << "../Input/aligned-ISO3200/1D2N-iso3200-";
+        fname << "../Input/aligned-ISO400/1D2N-iso400-under-";
+        // fname << "../Input/aligned-ISO3200/1D2N-iso3200-";
         fname << i;
         fname << ".png";
         seq.push_back(Image(fname.str()));
@@ -27,9 +28,23 @@ int main() {
 
     Image SNRIm = logSNR(seq,float(1/30.0));
     SNRIm.write("../Output/snr_map.png");
+    */
 
     Image raw = Image("../Input/raw/signs-small.png");
-    Image green = basicGreen(raw, 0);
+    raw.debug_write();
+    Image green = basicGreen(raw, 1);
     green.write("../Output/demosaic_green.png");
+    Image red = basicRorB(raw, 1, 1);
+    red.write("../Output/demosaic_red.png");
+    Image blue = basicRorB(raw, 0, 0);
+    blue.write("../Output/demosaic_blue.png");
+    
+    Image rgb = basicDemosaic(raw, 1, 1, 1, 0, 0);
+    rgb.write("../Output/demosaiced.png");
 
+    Image edge_green = edgeBasedGreen(raw, 1);
+    edge_green.write("../Output/demosaic_edge_green.png");
+
+    Image rgb_edge = edgeBasedGreenDemosaic(raw, 1, 1,1,0,0);
+    rgb_edge.write("../Output/demosaiced_edge.png");
 }
