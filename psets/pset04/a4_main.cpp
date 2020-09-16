eb0775dd83adc89643484413acd31c40ec500a77
vincent <vincentcheng929@gmail.com>
2020-09-16 09:26:54 +0800

    Update solution for pset04

---
+++ psets/pset04/a4_main.cpp
@@ -0,0 +1,63 @@
+#include "basic_image_manipulation.hpp"
+#include "filtering.hpp"
+#include "align.hpp"
+#include "demosaic.hpp"
+
+
+using namespace std;
+
+// This is a way for you to test your functions. 
+// We will only grade the contents of a0.cpp and Image.cpp
+int main() {
+    /*
+	// Denoise
+    vector<Image> seq;
+    int n_images = 44;
+    for (int i = 1; i <= n_images; ++i) {
+        ostringstream fname;
+        fname << "../Input/aligned-ISO400/1D2N-iso400-under-";
+        // fname << "../Input/aligned-ISO3200/1D2N-iso3200-";
+        fname << i;
+        fname << ".png";
+        seq.push_back(Image(fname.str()));
+    }
+    
+    Image output1 = denoiseSeq(seq);
+    output1.write("../Output/denoised.png");
+
+
+    Image SNRIm = logSNR(seq,float(1/30.0));
+    SNRIm.write("../Output/snr_map.png");
+
+    Image raw = Image("../Input/raw/signs-small.png");
+    Image green = basicGreen(raw, 0);
+    green.write("../Output/demosaic_green.png");
+
+    */
+
+    Image raw = Image("../Input/raw/signs-small.png");
+    raw.debug_write();
+    Image green = basicGreen(raw, 1);
+    green.write("../Output/demosaic_green.png");
+    Image red = basicRorB(raw, 1, 1);
+    red.write("../Output/demosaic_red.png");
+    Image blue = basicRorB(raw, 0, 0);
+    blue.write("../Output/demosaic_blue.png");
+    
+    Image rgb = basicDemosaic(raw, 1, 1, 1, 0, 0);
+    rgb.write("../Output/demosaiced.png");
+
+    Image edge_green = edgeBasedGreen(raw, 1);
+    edge_green.write("../Output/demosaic_edge_green.png");
+
+    Image rgb_edge = edgeBasedGreenDemosaic(raw, 1, 1, 1, 0, 0);
+    rgb_edge.write("../Output/demosaiced_edge.png");
+
+    Image edge_red = basicRorB(raw, 1, 1);
+    edge_red.write("../Output/demosaic_edge_red.png");
+    Image edge_blue = basicRorB(raw, 0, 0);
+    edge_blue.write("../Output/demosaic_edge_blue.png");
+
+    Image rgb_green_edge = improvedDemosaic(raw, 1, 1, 1, 0, 0);
+    rgb_green_edge.write("../Output/demosaic_improved.png");
+}
---
+++ psets/pset04/demosaic.cpp
@@ -0,0 +1,172 @@
+#include "demosaic.hpp"
+
+float interpGreen(const Image &raw, int x, int y) {
+	assert(x > 0 && x < raw.width() - 1);
+	assert(y > 0 && y < raw.height() - 1);
+
+	float interpVal = raw(x + 1, y) +
+					  raw(x - 1, y) +
+					  raw(x, y + 1) +
+					  raw(x, y - 1);
+	interpVal /= 4;
+	return interpVal;
+}
+
+Image basicGreen(const Image &raw, int offset)
+{
+	// If no offset, then 
+	// G N G N G N G
+	// N G N G N G N
+	// G N G N G N G
+	// Even rows have even greens
+	// Odd rows have odd greens
+	// With offset, even rows have odd greens, odd rows have even
+
+
+	Image output(raw.width(), raw.height(), 1);
+
+    bool condition;
+    if (offset % 2 == 0){
+        condition = true;
+    }
+    else {
+        condition = false;
+    }
+
+    for (int a = 1; a < raw.width() - 1; a++){
+        for (int b = 1; b < raw.height() - 1; b++){
+            if ((a % 2 == b % 2) == condition){
+                output(a,b) = raw(a,b);
+            }
+            else{
+                output(a,b) = (raw(a-1,b) + raw(a+1,b) + raw(a,b+1) + raw(a,b-1))/4.0; 
+            }
+        }
+    }
+    return output;
+}
+
+
+Image basicRorB(const Image &raw, int offsetX, int offsetY)
+{
+    Image output(raw.width(), raw.height(), 1);
+    for (int a = 1; a < raw.width() - 1; a++){
+        for (int b = 1; b < raw.height() - 1; b++){
+            if (a % 2 == offsetX && b % 2 == offsetY) {
+                output(a,b) = raw(a,b);
+            }
+            else if (a % 2 == offsetX && b % 2 != offsetY){
+                output(a,b) = (raw(a,b-1) + raw(a,b+1))/2.0;
+            }
+            else if (a % 2 != offsetX && b % 2 == offsetY){
+                output(a,b) = (raw(a-1,b) + raw(a+1,b))/2.0;
+            }
+            else{
+                output(a,b) = (raw(a-1,b-1) + raw(a-1,b+1) + raw(a+1,b-1) + raw(a+1,b+1))/4.0; 
+            }
+        }
+    }
+    return output;
+}
+
+Image basicDemosaic(const Image &raw, int offsetGreen, int offsetRedX, int offsetRedY, int offsetBlueX, int offsetBlueY)
+{
+	Image green = basicGreen(raw, offsetGreen);
+	Image red = basicRorB(raw, offsetRedX, offsetRedY);
+	Image blue = basicRorB(raw, offsetBlueX, offsetBlueY);
+	Image output(raw.width(), raw.height(), raw.channels());
+
+
+	for (int h = 0; h < raw.height(); h++) {
+		for (int w = 0; w < raw.width(); w++) {
+			output(w, h, 0) = red(w, h);
+			output(w, h, 1) = green(w, h);
+			output(w, h, 2) = blue(w, h);
+		}
+	}
+
+	return output;
+}
+
+Image edgeBasedGreen(const Image &raw, int offset)
+{
+	Image output(raw.width(), raw.height(), 1);
+
+    bool condition;
+    if (offset % 2 == 0){
+        condition = true;
+    }
+    else {
+        condition = false;
+    }
+
+    for (int a = 1; a < raw.width() - 1; a++){
+        for (int b = 1; b < raw.height() - 1; b++){
+            if ((a % 2 == b % 2) == condition){
+                output(a,b) = raw(a,b);
+            }
+            else if (abs(raw(a-1,b) - raw(a+1,b)) > abs(raw(a,b+1) - raw(a,b-1))) {
+                output(a,b) = (raw(a, b+1) + raw(a, b-1))/2.0; 
+            } else {
+            	output(a,b) = (raw(a-1, b) + raw(a+1, b))/2.0; 
+            }
+        }
+    }
+
+    return output;
+}
+
+Image edgeBasedGreenDemosaic(const Image &raw, int offsetGreen, int offsetRedX, int offsetRedY, int offsetBlueX, int offsetBlueY)
+{
+	Image green = edgeBasedGreen(raw, offsetGreen);
+	Image red = basicRorB(raw, offsetRedX, offsetRedY);
+	Image blue = basicRorB(raw, offsetBlueX, offsetBlueY);
+	Image output(raw.width(), raw.height(), raw.channels());
+
+
+	for (int h = 0; h < raw.height(); h++) {
+		for (int w = 0; w < raw.width(); w++) {
+			output(w, h, 0) = red(w, h);
+			output(w, h, 1) = green(w, h);
+			output(w, h, 2) = blue(w, h);
+		}
+	}
+
+	return output;
+
+}
+
+Image greenBasedRorB(const Image &raw, Image &green, int offsetX, int offsetY)
+{
+    Image three_channel_green = raw;
+    for (int h = 0; h < raw.height(); h++) {
+        for (int w = 0; w < raw.width(); w++) {
+            for (int c = 0; c < raw.channels(); c++){
+                three_channel_green(w, h, c) = green(w, h);
+            }
+        }
+    }
+    Image raw_minus_green = raw - three_channel_green;
+    Image basic_r_or_b_minus_green = basicRorB(raw_minus_green, offsetX, offsetY);
+    Image greenBasedRorB_im = basic_r_or_b_minus_green + green;
+    return greenBasedRorB_im;
+}
+
+Image improvedDemosaic(const Image &raw, int offsetGreen, int offsetRedX, int offsetRedY, int offsetBlueX, int offsetBlueY)
+{
+    Image green = edgeBasedGreen(raw, offsetGreen);
+    Image red = greenBasedRorB(raw, green, offsetRedX, offsetRedY);
+    Image blue = greenBasedRorB(raw, green, offsetBlueX, offsetBlueY);
+    Image output(raw.width(), raw.height(), raw.channels());
+
+    for (int h = 0; h < raw.height(); h++) {
+        for (int w = 0; w < raw.width(); w++) {
+            output(w, h, 0) = red(w, h);
+            output(w, h, 1) = green(w, h);
+            output(w, h, 2) = blue(w, h);
+        }
+    }
+
+    return output;
+}
+
